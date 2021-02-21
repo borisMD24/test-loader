82ef9171128d132dbfcfaf730a265e832c7ecc9b
rtdeedqgryet <borismariondorier@gmail.com>
2021-02-21 12:51:10 +0100

    first

---
+++ js/main.js
@@ -0,0 +1,19 @@
+/*      _________________     _ _.-''''''--. 
+        FFFFFFFFFFFreedom   .` `.  ...------.\   
+       /FFFFFFFFFFFactory  / |O :-`   _,.,---.)
+       FFFFF/              '     ;--''      
+      /FFFFF               | _.' (        
+      FFFFFFFFFFF          ,' _,' `-.
+     /FFFFFFFFFF/          : /       '.
+     FFFFF/                    code  ' 
+    /FFFFF                  `.|    to   `-.
+    FFFFF/                    `-._  be    \
+   /FFFFF                         '.  ,-.  \
+   FFFFF/                          _`...` \
+   FFFFF                       .,-'.,-'  ``: \
+ ==============================((=((======== \== bobo le plus beau fait du js
+                                              \
+                                               \
+                                              ` \
+                                                -*/
+ 
