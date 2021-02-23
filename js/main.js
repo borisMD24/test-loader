c083cf031b6386d9f735f49e78bf60aa823b1121
Boris Marion-Dorier <borismariondorier@gmail.com>
2021-02-23 22:14:16 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
-window.addEventListener("load", () => {
-  prompt();
-})
-
-async function prompt () {
-  let img = document.querySelectorAll("img");
-  for(let i = 0; i < img.length; i++){
-    console.log(img[i]);
-    img[i].style.transform = "rotateX(0deg)";
-    await sleep(250);
+const reveal = async () => {
+  let tr = Array.from(document.querySelectorAll("svg image"));
+  let cnt = 0;
+  while(tr.length >= 1){
+    let n = Math.floor(Math.random() * Math.floor(tr.length));
+    tr[n].style.opacity = "1"
+    tr.splice(n, 1);
+    await sleep (tr.length/(75*tr.length/100));
   }
 }
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
+reveal();
