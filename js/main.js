6f4f0c674991b720664a6ce5bf871475080646aa
Boris Marion-Dorier <borismariondorier@gmail.com>
2021-11-20 04:40:44 +0100

    got rid of timeout

--- js/main.js
+++ js/main.js
@@ -1,23 +1,27 @@
 class svgImage {
   constructor(element){
     this.element = element;
-    element.style.transitionDelay = "0s";
-    element.style.transitionDuration = "0s";
+    element.style.transitionDelay = "0";
+    element.style.transitionDuration = "0";
     this.baseValCp = {}
     const originalTransforms = this.getTranslateXY(element.transform.baseVal[0].matrix);
     this.x = originalTransforms.translateX;
     this.y = originalTransforms.translateY;
     this.setUp();
+    let self = this;
+    setTimeout(()=>{
+      self.setElementTransition()
+    }, 100);
   }
   setUp(){
-    const yDir = ((Math.random()>=0.5)? 1 : 0) * 2 - 1
-    const xDir = ((Math.random()>=0.5)? 1 : 0) * 2 - 1
     const zDir = ((Math.random()>=0.5)? 1 : 0) * 2 - 1
 
-    this.element.style.transform = `translate(${Math.random()*100}vw, ${Math.random()*100}vh)`+` translateZ(${zDir * Math.random()*50}vh) rotateX(${Math.random()*360}deg) rotateY(${Math.random()*360}deg)`;
-    this.element.style.transitionDelay = `${Math.random()*2}s`
-    this.element.style.transitionDuration = `${Math.random()*2}s`
+    this.element.style.transform = `translate(${Math.random()*100}vw, ${Math.random()*100}vh)`+` rotateX(${Math.random()*1080}deg) rotateY(${Math.random()*1080}deg) scale(${Math.random()*5})`;
     this.element.style.opacity = "0"
+  }
+  setElementTransition(){
+    this.element.style.transitionDelay = `${Math.random()*3}s`
+    this.element.style.transitionDuration = `${Math.random()*2}s`
   }
   getTranslateXY(m) {
     const matrix = new DOMMatrixReadOnly(`matrix(${m.a}, ${m.b}, ${m.c}, ${m.d}, ${m.e}, ${m.f})`)
@@ -27,9 +31,11 @@ class svgImage {
       }
   }
   reshape(){
-    console.log(this.x);
-    this.element.style.transform = `translateX(${this.x}px) translateY(${this.y}px)`
-    this.element.style.opacity = "1"
+    setTimeout(()=>{
+      console.log(this.x);
+      this.element.style.transform = `translateX(${this.x}px) translateY(${this.y}px)`
+      this.element.style.opacity = "1"
+    }, 300)
   }
 }
 
@@ -61,7 +67,7 @@ const sleep = (milliseconds) => {
   return new Promise(resolve => setTimeout(resolve, milliseconds))
 }
 async function reveal(){
-  await sleep(2000);
+  //await sleep(2000);
   scene.trigger();
 }
 reveal()
